4bfbb23bb7aa96af67a24838310f054dac67b019
Daniel <djrenjiffo@terra.com.br>
2015-09-04 15:05:01 -0300

    Ep1....

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -15,12 +15,19 @@ typedef struct
 	char prior; /*-19 a 20*/
 }trace;
 /*---------------------------------*/
-trace processTable[MAX_THREADS];
+  pthread_t threadID[MAX_THREADS];
 
+struct arg_struct {
+    int delay;
+    int id;
+};
+trace processTable[MAX_THREADS];
+pthread_mutex_t *mutex;
 
 void showPT(trace processTable[], int size);
 void getTokens(char *line, trace *process);
-void *counter(void *void_delay);
+void *first_counter(void *void_delay);
+void *counter(void *arguments);
 
 
 int main(int argc, char *argv[]) {
@@ -31,9 +38,11 @@ int main(int argc, char *argv[]) {
     char *token;
     int id;
     
-    clock_t t,now;
-    t = clock();
-
+    clock_t t;
+    long now;
+    
+    struct arg_struct args;  
+ 
     /*Verifica entrada*/
     if (argc < 4) {
     	printf("USAGE: ./ep1 x(1 to 6) traceFile resultFile -d(optional)\n");
@@ -57,24 +66,50 @@ int main(int argc, char *argv[]) {
        getTokens(line, &processTable[size -1]);
   }
   /*---------------------------------------------------*/
-  pthread_t threadID[size + 1];
+  //pthread_t threadID[size + 1];
 
   switch(id) {
     /*Escalanador first in first out*/
     case 1:
     y = 0;
+    t = clock();
     while (1) {
-    now =  (double)(clock() - t) / CLOCKS_PER_SEC;
-    if ( now == processTable[y].init) {
-       if (pthread_create(&threadID, NULL, counter, &processTable[y].dt) == 0) {
-          printf("DEU RUIM\n");
-          exit(0);
-       }
+      now =  (long)(clock() - t) / CLOCKS_PER_SEC; 
+       
+      if ( now == processTable[y].init && y == 0) {
+         if (pthread_create(&threadID[y], NULL, first_counter, (void *) &processTable[y].dt) != 0) {
+           printf("ERROR\n");
+           exit(0);
+         }
+          
+         printf("Thread[%d] inicializada\n",y);
+      
+         y++;
+      }
+      if ( now == processTable[y].init && y > 0) {
+          args.delay = processTable[y].dt;
+          args.id = (y - 1);
+          if (pthread_create(&threadID[y], NULL, counter, (void *) &args) != 0) {
+           printf("ERROR\n");
+           exit(0);
+         }
+          
+         printf("Thread[%d] inicializada\n",y); 
+         y++;
+      }
+    if ( y == size) {
+       printf("Ultimo, vou esperar...\n");
+       for (x = 0; x < size; x ++)
+          pthread_join(threadID[x],NULL);
+       printf(".....Vixes ja foi\n");
+       break;
+    } 
+    
     }
-    }
+    
     break;
-    }  
-  } 
+  }  
+   
   return (0);
  
 }
@@ -129,19 +164,42 @@ void getTokens(char *line, trace *process) {
     y++;
   }
   process->prior = atoi(buffer);  
-  
 }
 
-void *counter(void *void_delay) {
-  float *delay = (float *)void_delay; 
+void *first_counter(void *void_delay) {
+  float delay = *((float *)(void_delay)); 
   clock_t t,now;
   t = clock();
   while (1) {
     now =  (double)(clock() - t) / CLOCKS_PER_SEC;
-    if ( (float *)now >= delay) {
+    if ( now >= delay) {
      break; 
         }
    }
-  printf("Thread Acabada!\n");
+  printf("---Thread[0] Acabou!---\n");
+  return NULL;
+}
+
+void *counter(void *arguments) {
+  struct arg_struct *args = arguments;
+  /*Espera a anterior acabar..*/
+  printf("Esperando a  thread[%d] acabar...\n", args->id);
+  if (pthread_join(threadID[args->id],NULL) != 0) { 
+    printf("DEU RUIM\n");
+    pthread_exit(NULL);
+  }
+  printf("Acabou\n");
+  clock_t t,now;
+  t = clock();
+  pthread_mutex_lock(&mutex);
+  while (1) {
+    now =  (double)(clock() - t) / CLOCKS_PER_SEC;
+    if ( now >= args->delay) {
+     break; 
+        }
+   }
+  pthread_mutex_unlock(&mutex);
+  printf("---Thread[%d] Acabada!---\n",(args->id + 1));
+  pthread_exit(NULL);
   return NULL;
 }
