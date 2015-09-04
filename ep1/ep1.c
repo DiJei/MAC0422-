34469f76954aeb92eb2d39d3fb6ea6ef7de0e4fb
DiJei <djrenjiffo@gmail.com>
2015-09-03 22:46:05 -0300

    Primeiro escalonador

--- ep1/ep1.c
+++ ep1/ep1.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 
 #define MAX_THREADS 100
 #define MAX_LINE 100
@@ -19,6 +20,7 @@ trace processTable[MAX_THREADS];
 
 void showPT(trace processTable[], int size);
 void getTokens(char *line, trace *process);
+void *counter(void *void_delay);
 
 
 int main(int argc, char *argv[]) {
@@ -27,18 +29,24 @@ int main(int argc, char *argv[]) {
     int size = 0;
     char line[MAX_LINE];
     char *token;
+    int id;
+    
+    clock_t t,now;
+    t = clock();
+
     /*Verifica entrada*/
     if (argc < 4) {
     	printf("USAGE: ./ep1 x(1 to 6) traceFile resultFile -d(optional)\n");
     	exit(0);
     }
+    id = atoi(argv[1]);
+
     FILE *trace;
     if ((trace = fopen(argv[2],"r")) == NULL) {
        	printf("arquivo:%s nao encontrado\n",argv[2]);
         exit(0);
     }
 	/*---------------*/
-  
   /*Ler o arquivo trace e monta a tabela de processo*/
   for (x = 0; x < MAX_LINE; x++)
     	line[x] = 0;
@@ -48,15 +56,29 @@ int main(int argc, char *argv[]) {
        size++;
        getTokens(line, &processTable[size -1]);
   }
-	showPT(processTable,size);
   /*---------------------------------------------------*/
-  
-  switch(argc) {
-    case 1:
+  pthread_t threadID[size + 1];
+
+  switch(id) {
     /*Escalanador first in first out*/
+    case 1:
+    y = 0;
+    while (1) {
+    now =  (double)(clock() - t) / CLOCKS_PER_SEC;
+    if ( now == processTable[y].init) {
+       if (pthread_create(&threadID, NULL, counter, &processTable[y].dt) == 0) {
+          printf("DEU RUIM\n");
+          exit(0);
+       }
+    }
+    }
+    break;
+    }  
   } 
   return (0);
+ 
 }
+
 
 void showPT(trace processTable[], int size) {
 	printf("init |  name |  dt  | deadline  |  prior |\n");
@@ -108,4 +130,18 @@ void getTokens(char *line, trace *process) {
   }
   process->prior = atoi(buffer);  
   
+}
+
+void *counter(void *void_delay) {
+  float *delay = (float *)void_delay; 
+  clock_t t,now;
+  t = clock();
+  while (1) {
+    now =  (double)(clock() - t) / CLOCKS_PER_SEC;
+    if ( (float *)now >= delay) {
+     break; 
+        }
+   }
+  printf("Thread Acabada!\n");
+  return NULL;
 }
